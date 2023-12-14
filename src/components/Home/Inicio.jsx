16e4c950ac9c607908811263b27ef16dfb5225a1
Muertoparado <owellandry10@gmail.com>
2023-12-13 21:24:19 -0500

    Update all files

--- src/components/Home/Inicio.jsx
+++ src/components/Home/Inicio.jsx
@@ -1,5 +1,6 @@
 // src/components/Home/Home.jsx
 import React from 'react';
+import './Inicio.css';
 
 const Home = () => {
   
@@ -13,18 +14,41 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Página de Inicio</h1>
-      <p>¡Bienvenido a tu página de inicio!</p>
-      <div>
-        <h2>Información del Usuario</h2>
-        <p>Nombre: {usuarioRandom.nombre}</p>
-        <p>Apellido: {usuarioRandom.apellido}</p>
-        <p>Edad: {usuarioRandom.edad}</p>
+      <div className='display'>
+
+        <h1>Página de Inicio</h1>
+          <p>¡Bienvenido a tu página de inicio!</p>
+        <div>
+          <h2>Información del Usuario</h2>
+            <p>Nombre: {usuarioRandom.nombre}</p>
+            <p>Apellido: {usuarioRandom.apellido}</p>
+            <p>Edad: {usuarioRandom.edad}</p>
+        </div>
+        <div>
+          <a href={endpointUsuario} target="_blank" rel="noopener noreferrer">Abrir en nueva pestaña</a>
+        </div>
+
+      
       </div>
-      <div>
-        <a href={endpointUsuario} target="_blank" rel="noopener noreferrer">Abrir en nueva pestaña</a>
+
+      <div className='di'>
+        <div className='derdi'>
+          <div>
+            <div>
+              <img className='user-image' src="https://img.icons8.com/?size=256&id=AZazdsitsrgg&format=png" alt="User" />
+            </div>
+            <div>
+              <h3>Nombre</h3>
+              <h3>Apellido</h3>
+            </div>
+          </div>
+        </div>
+        <div className='izdi'></div>
       </div>
+
+
     </div>
+    
   );
 };
 
