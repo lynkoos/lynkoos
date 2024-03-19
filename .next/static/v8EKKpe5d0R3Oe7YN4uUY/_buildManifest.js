dc27afbf6cff046113a2dfed8baa6eab00c37471
owell andry <owellandry10@gmail.com>
2024-03-19 04:17:59 -0500

    Eliminate all content from master branch

--- .next/static/v8EKKpe5d0R3Oe7YN4uUY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-a0f9c1fe45b6d9fb.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
