48a3f645798d8806545f5a47c9b3958deb9ee63b
owell andry <owellandry10@gmail.com>
2024-03-20 16:34:59 -0500

    Update changes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,28 @@
-// app/page.tsx
-
-import "@/app/global/globals.css"
-import "./css/404.css"
+import Link from 'next/link'
+import "@/app/global/globals.css";
+import "./css/404.css";
 
 export default function NotFoundPage() {
   return (
     <div className="NotFoundContPrin">
-        <div className="NotFoundConent">
-          <h3 className="NotFoundH3">404</h3>
-          <div className="NotFoundBackH3"></div>
-          <div>
-            <span>El enlace al que intentas acceder no se ha pidido encontrar, por favor, verifica el enlace</span>
+      <div className="NotFoundConent">
+        <article className="card">
+          <div className="card-inner">
+            <span className="card-pin"></span>
+            <h3 className="card-title">404</h3>
+            <div className="card-content">
+              <span className="card-meta-number">El enlace al que intentas acceder no se ha podido encontrar. Por favor, verifica el enlace.</span>
+            </div>
+            <div className="card-pin"></div>
           </div>
+        </article>
+        <div className="NotFoundBack"></div>
+        <div className="NotFoundContBack">
+          <Link href="/">
+            <span className='NotFoundContSpan'>volver</span>
+          </Link>
         </div>
+      </div>
     </div>
   );
 }
